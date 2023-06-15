6705076c8540c660ebcef98351a142f9ca7df050
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-06-15 12:37:30 +0100

    ⌨ Added key event listeners in the stopwatch project.

--- stopwatch.js
+++ stopwatch.js
@@ -23,38 +23,57 @@ if (timer && buttonState !== "Go") handleGoButtonClick();
 const addLapButton = document.querySelector("button.js-add-lap-button");
 const lapList = document.querySelector("ol.js-lap-list");
 
-addLapButton.addEventListener("click", () => {
-	lapList.innerHTML += `<li class="lap-item">${formatTime(timer)}</li>`;
-});
+addLapButton.addEventListener("click", addLap);
 
 const resetButton = document.querySelector("button.js-reset-button");
 
-resetButton.addEventListener("click", () => {
-	clearInterval(interval);
-	interval = null;
-
-	startTime = null;
-	timer = 0;
-
-	updateTimerElement();
-	if (buttonState === "Stop") handleGoButtonClick();
-	lapList.innerHTML = "";
+document.body.addEventListener("keydown", (event) => {
+	switch (event.key) {
+		case "g":
+			if (buttonState === "Go") handleGoButtonClick();
+			break;
+		case "s":
+			if (buttonState === "Stop") handleGoButtonClick();
+			break;
+		case "a":
+			addLap();
+			break;
+		case "Backspace":
+			showResetConfirmation();
+			break;
+		case "r":
+			showResetConfirmation();
+			break;
+		case "y":
+			document.querySelector("button.js-yes-button") &&
+				(clearConfirmation() || resetTimer());
+			break;
+		case "n":
+			document.querySelector("button.js-no-button") && clearConfirmation();
+			break;
+	}
 });
 
+resetButton.addEventListener("click", showResetConfirmation);
+
+const confirmationContainer = document.querySelector(
+	"div.js-confirmation-container",
+);
+
 function handleGoButtonClick() {
-	if (goButton.innerHTML === "Go") {
+	if (goButton.innerHTML === "Go (G)") {
 		startTime = Date.now() - timer;
 
 		interval = setInterval(updateTimerElement, 0);
 
-		goButton.innerHTML = "Stop";
+		goButton.innerHTML = "Stop (S)";
 		localStorage.setItem("buttonState", "Stop");
 		buttonState = "Stop";
 	} else {
 		clearInterval(interval);
 		interval = null;
 
-		goButton.innerHTML = "Go";
+		goButton.innerHTML = "Go (G)";
 		localStorage.setItem("buttonState", "Go");
 		buttonState = "Go";
 	}
@@ -85,4 +104,46 @@ function formatTime(time) {
 
 function updateTimerElement() {
 	timerElement.innerHTML = formatTime();
+}
+
+function addLap() {
+	lapList.innerHTML += `<li class="lap-item">${formatTime(timer)}</li>`;
+}
+
+function resetTimer() {
+	clearInterval(interval);
+	interval = null;
+
+	startTime = null;
+	timer = 0;
+
+	updateTimerElement();
+	if (buttonState === "Stop") handleGoButtonClick();
+	lapList.innerHTML = "";
+}
+
+function showResetConfirmation() {
+	confirmationContainer.innerHTML = `
+
+	<div>Are you sure you want to reset the timer?</div>
+	<button class="yes-button js-yes-button">Yes (Y)</button>
+	<button class="no-button js-no-button">No (N)</button>
+
+	`;
+
+	const noButton = document.querySelector("button.js-no-button");
+
+	noButton.addEventListener("click", clearConfirmation);
+
+	const yesButton = document.querySelector("button.js-yes-button");
+
+	yesButton.addEventListener("click", () => {
+		clearConfirmation();
+
+		resetTimer();
+	});
+}
+
+function clearConfirmation() {
+	confirmationContainer.innerHTML = "";
 }
